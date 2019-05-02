567c0cad80111d09a576b7bce69f1a59a9d1f23d
xujianpeng <13023209562@163.com>
2019-05-02 12:27:29 +0800

    删除

--- Day08_26/src/com/itheima/ber/ver/Ser/ss/Sw.java
+++
@@ -1,8 +0,0 @@
-package com.itheima.ber.ver.Ser.ss;
-
-/**
- * @author DELL
- * @date 2018--09--19--22:42
- */
-public class Sw {
-}
--- Day08_26/src/com/itheima/num1/basics/extension/Sport.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.num1.basics.extension;
-
-/**
- * @author DELL
- * @date 2018--08--26--18:01
- */
-public interface Sport {
-    void run();
-}
